f5f4a4c92265a070343e44a89a1f554a63bdc156
root <root@vmi3031941.contaboserver.net>
2026-01-20 18:41:38 +0100

    Initial docker infra: nginx + nest + postgres + redis

---
+++ api/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): any {
+    return {
+      ok: true,
+      service: "taxi_api",
+      message: "API is running ✅",
+    };
+  }
+}
---
+++ api/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+}
+bootstrap();
